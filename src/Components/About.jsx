997b3cae294067394962c6013a54843186c3432d
Maycol1490 <maicol1490@hotmail.com>
2023-01-30 15:07:53 -0500

    Cambios

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import fotoPerfil from '../assets/logo-f.png'
+import noticia1 from '../assets/1logo-nav.png'
+import noticia2 from '../assets/fondo-home.jpg'
+import noticia3 from '../assets/fondo-perfil.jpg'
+import noticia4 from '../assets/logo-.jpeg'
+import noticia5 from '../assets/fondo-perfil1.jpg'
+
 
 const About = () => {
   return (
@@ -11,13 +18,32 @@ const About = () => {
       </div>
       <div>
         <div class="card">
-          <div class="card_load"></div>
+          <div class="card_load">
+            <img src={fotoPerfil} alt="" />
+          </div>
           <div>
-          <div class="card_load_extreme_title">Alarcon & Asociados </div>
-          <div class="card_load_extreme_descripion">Alarcon & Abogados Asociados, es una firma de especialistas en distintas áreas del derecho enfocados en adelantar asesorías y representaciones judiciales exitosas, reconocidos por su alto compromiso y empeño en casos de alta complejidad a nivel nacional.
-          Contamos con aliados estratégicos y profesionales capacitados en dar acompañamiento a cada una de las etapas del proceso judicial que usted enfrenta.
+            <div class="card_load_extreme_title">
+              <h1>
+                Alarcon & Asociados
+              </h1>
+            </div>
+            <div class="card_load_extreme_descripion">
+              <p>
+              Alarcon & Abogados Asociados, es una firma de especialistas en distintas áreas del derecho enfocados en adelantar asesorías y representaciones judiciales exitosas, reconocidos por su alto compromiso y empeño en casos de alta complejidad a nivel nacional.
+              Contamos con aliados estratégicos y profesionales capacitados en dar acompañamiento a cada una de las etapas del proceso judicial que usted enfrenta.
+              </p>
+            </div>
           </div>
-          </div>
+        </div>
+      </div>
+      <div className='noticias-perfil'>
+        <h3 className='subtitle-perfil'>Noticias</h3>
+        <div className='galeria-perfil'>
+          <img src={noticia1} alt="" />
+          <img src={noticia2} alt="" />
+          <img src={noticia3} alt="" />
+          <img src={noticia4} alt="" />
+          <img src={noticia5} alt="" />
         </div>
       </div>
     </section>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Contact = () => {
   return (
     <section className='container-contacto'>
@@ -13,6 +14,9 @@ const Contact = () => {
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.330548500993!2d-76.54343182724736!3d3.451171160005551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6650189578f%3A0x26eb5e6da6531d3f!2sCra.%204%20%238-63%2C%20San%20Pedro%2C%20Cali%2C%20Valle%20del%20Cauca!5e0!3m2!1ses-419!2sco!4v1673892794080!5m2!1ses-419!2sco"
           style={{ width: '500px', height: '500px' }}></iframe>
       </div>
+    </div>
+    <div>
+
     </div>
     </section >
   )
