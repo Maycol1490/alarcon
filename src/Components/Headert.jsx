9c9cd714fc9c01be46c0b3efce51473f30d29852
Maycol1490 <maicol1490@hotmail.com>
2023-01-30 21:20:05 -0500

    header

--- src/Components/Headert.jsx
+++ src/Components/Headert.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../styles/Headert.css'
 import { NavLink } from 'react-router-dom'
+import { FaBars} from "react-icons/fa";
 import Logo from '../assets/1logo-nav.png'
+import { useState } from 'react';
 
 
 const Headert = () => {
+  const [nav, setnav] = useState(false)
+  
+  
+  const handleNav = () => {
+    setnav(true)
+  }
 
   return (
     <header className='header'>
@@ -17,6 +26,7 @@ const Headert = () => {
           <li><NavLink to='/Contact'>Contacto</NavLink></li>
         </ul>
       </nav>
+      <div className='nav-movil'><FaBars onClick={handleNav}/></div>
     </header>
   )
 }
