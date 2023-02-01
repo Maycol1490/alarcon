87255125e33efaad0cce4abb0627ace635aaf36f
Maycol1490 <maicol1490@hotmail.com>
2023-02-01 16:31:28 -0500

    perfil

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/About.css'
 import fotoPerfil from '../assets/logo-f.png'
 import noticia1 from '../assets/1logo-nav.png'
 import noticia2 from '../assets/Fondo-home.jpg'
@@ -11,7 +12,6 @@ const About = () => {
   return (
     <section>
       <div className='perfil'>
-        <h1 className='title-perfil'>Quienes Somos</h1>
         <p className='descripcion-perfil'>Alarcon & Abogados Asociados, es una firma de especialistas en distintas áreas del derecho enfocados en adelantar asesorías y representaciones judiciales exitosas, reconocidos por su alto compromiso y empeño en casos de alta complejidad a nivel nacional.
           Contamos con aliados estratégicos y profesionales capacitados en dar acompañamiento a cada una de las etapas del proceso judicial que usted enfrenta.
         </p>
