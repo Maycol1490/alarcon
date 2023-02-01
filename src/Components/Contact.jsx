bae142dd91bd9ebeb26a505d9ab6e0eb8ef887f5
Maycol1490 <maicol1490@hotmail.com>
2023-02-01 17:24:59 -0500

    contacto

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
         </div>
         <div className='mapa-contacto'>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15930.330548500993!2d-76.54343182724736!3d3.451171160005551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6650189578f%3A0x26eb5e6da6531d3f!2sCra.%204%20%238-63%2C%20San%20Pedro%2C%20Cali%2C%20Valle%20del%20Cauca!5e0!3m2!1ses-419!2sco!4v1673892794080!5m2!1ses-419!2sco"
-            style={{ width: '100%', height: '500px' }}></iframe>
+          ></iframe>
         </div>
       </div>
     </section >
