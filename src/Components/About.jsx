8d432ab41d25cfa5b8dbd692d169bfbbde94c627
Maycol1490 <maicol1490@hotmail.com>
2023-02-11 12:48:42 -0500

    rr

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../styles/About.css'
 import fotoPerfil from '../assets/logo-.jpeg'
-import noticia1 from '../assets/1logo-nav.png'
-import noticia2 from '../assets/imagen1.jpg'
-import noticia3 from '../assets/imagen2.jpg'
-import noticia4 from '../assets/imagen3.jpg'
-import noticia5 from '../assets/imagen4.jpg'
+import noticia1 from '../assets/video1.mp4'
+import noticia2 from '../assets/video2.mp4'
+import noticia3 from '../assets/video3.mp4'
+import noticia4 from '../assets/video4.mp4'
+
 
 
 const About = () => {
@@ -19,13 +19,13 @@ const About = () => {
           <div>
             <div class="card_load_extreme_title">
               <h1>
-               Camilo Alarcon
+                Camilo Alarcon
               </h1>
             </div>
             <div class="card_load_extreme_descripion">
               <p>
-              Alarcon & Abogados Asociados, es una firma de especialistas en distintas áreas del derecho enfocados en adelantar asesorías y representaciones judiciales exitosas, reconocidos por su alto compromiso y empeño en casos de alta complejidad a nivel nacional.
-              Contamos con aliados estratégicos y profesionales capacitados en dar acompañamiento a cada una de las etapas del proceso judicial que usted enfrenta.
+                Alarcon & Abogados Asociados, es una firma de especialistas en distintas áreas del derecho enfocados en adelantar asesorías y representaciones judiciales exitosas, reconocidos por su alto compromiso y empeño en casos de alta complejidad a nivel nacional.
+                Contamos con aliados estratégicos y profesionales capacitados en dar acompañamiento a cada una de las etapas del proceso judicial que usted enfrenta.
               </p>
             </div>
           </div>
@@ -34,11 +34,18 @@ const About = () => {
       <div className='noticias-perfil'>
         <h3 className='subtitle-perfil'>Noticias</h3>
         <div className='galeria-perfil'>
-          <img src={noticia1} alt="" />
-          <img src={noticia2} alt="" />
-          <img src={noticia3} alt="" />
-          <img src={noticia4} alt="" />
-          <img src={noticia5} alt="" />
+          <video controls>
+            <source src={noticia1} type="video/mp4" />
+          </video>
+          <video controls>
+            <source src={noticia2} type="video/mp4" />
+          </video>
+          <video controls>
+            <source src={noticia3} type="video/mp4" />
+          </video>
+          <video controls>
+            <source src={noticia4} type="video/mp4" />
+          </video>
         </div>
       </div>
     </section>
