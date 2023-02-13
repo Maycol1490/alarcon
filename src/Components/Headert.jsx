640541c7c276573a82aebc597405bc6f284a41ca
Maycol1490 <maicol1490@hotmail.com>
2023-02-13 18:16:18 -0500

    nav

--- src/Components/Headert.jsx
+++ src/Components/Headert.jsx
@@ -9,13 +9,14 @@ import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs"
 
 const Headert = () => {
   const [navMobil, setnavMobil] = useState(false)
+  const [navOpen, setnavOpen] = useState(false)
 
   //e.target.reset()
 
   const handleNav = () => {
-    setnavMobil(!navMobil)
+    setnavMobil(!navMobil);
+
   }
-  console.log(navMobil);
 
   return (
     <header className='header'>
@@ -31,14 +32,22 @@ const Headert = () => {
         <div className='imangen-logo'>
           <NavLink to='/Home'><img src={Logo} alt="Alarcon Y Asociados" /></NavLink>
         </div>
-        <nav className={navMobil ? 'nav-mobil' : 'nav'}>
+        <nav className='nav'>
           <ul>
             <li><NavLink to='/About'>Quienes Somos</NavLink></li>
             <li><NavLink to='/Servicios'>Areas De Práctica</NavLink></li>
-            <li><NavLink to='/Contact'>Contacto</NavLink></li>
+            <li><NavLink to='/Contact' >Contacto</NavLink></li>
           </ul>
         </nav>
-        <div className='nav-movil'><FaBars onClick={handleNav} /></div>
+        <div className='nav-movil'><FaBars onClick={handleNav} />
+        <nav className={`'nav-mobil' ${navMobil ? 'nav-mobil' : 'nav-none'}`}>
+          <ul>
+            <li><NavLink to='/About' onClick={handleNav}>Quienes Somos</NavLink></li>
+            <li><NavLink to='/Servicios' onClick={handleNav}>Areas De Práctica</NavLink></li>
+            <li><NavLink to='/Contact' onClick={handleNav}>Contacto</NavLink></li>
+          </ul>
+        </nav>
+        </div>
       </div>
     </header>
   )
