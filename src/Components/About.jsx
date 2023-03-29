0b2f962442602b537cda079c62c646658899b017
Maycol1490 <maicol1490@hotmail.com>
2023-03-28 21:49:06 -0500

    perfiles

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
           <div>
             <div className="card_load_extreme_title">
               <h1>
-                Camilo Alarcon
+                Camilo Alarcon Perdomo.
               </h1>
             </div>
             <div className="card_load_extreme_descripion">
@@ -27,6 +27,48 @@ const About = () => {
               <p>
               Abogado de la universidad Libre, especialista en Derecho Penal y Criminalística de la misma universidad y especialista en Derecho Tributario de la Universidad Externado de Colombia. Actualmente se desempeña como Director General de la firma Alarcón & Asociados. Ha sido defensa de confianza en procesos penales complejos y de relevancia nacional. 
               Es preciso exaltar su conocimiento y experiencia en los asuntos jurídicos en materia penal por su gran capacidad de oratoria y argumentación.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="card_load">
+            <img src={fotoPerfil} alt="" />
+          </div>
+          <div>
+            <div className="card_load_extreme_title">
+              <h1>
+                Mauricio Martinez 
+              </h1>
+            </div>
+            <div className="card_load_extreme_descripion">
+              <h4>Asociados</h4>
+              <p>
+              Abogado egresado de la Universidad Libre, Litigante en derecho privado con énfasis en la defensa jurídica en procesos de carácter judicial, Actualmente se desempeña como asesor externo de varias empresas de la región, asesor principal externo de profesionales asociados empresa dedicada a la recuperación de cartera y 
+              asesoría jurídica bajo la lay 675 de 2001, como profesional se destaca su capacidad de análisis de las causas que motivan el servicio para establecer la mejor estrategia y asesorarlo durante todo el proceso, tomando los asuntos a él conferidos con responsabilidad y dinamismo, gracias su experiencia cuenta con una alta valoración de sus clientes siendo un sello de respaldo para estos.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <div className="card_load">
+            <img src={fotoPerfil} alt="" />
+          </div>
+          <div>
+            <div className="card_load_extreme_title">
+              <h1>
+                Amir Abdul Rahim Lopez 
+              </h1>
+            </div>
+            <div className="card_load_extreme_descripion">
+              <h4>Asociados</h4>
+              <p>
+              Abogado egresado de la universidad Libre de Cali, especialista en Derecho Penal y Magister en criminalística y ciencias forenses de la misma universidad, Actualmente se desempeña como defensor de confianza en procesos penales en casos de conocimiento público, desarrollando defensas estratégicas sólidas a partir de pruebas periciales  científicas las cuales brindan plena garantía ante órganos de cierre como la Corte Suprema de Justicia, donde 
+              ha tenido la oportunidad de manejar casos en delitos como Concierto para delinquir, narcotráfico, lavado de activos, porte de armas de uso privativo, tráfico de estupefacientes, terrorismo, delitos sexuales y procesos de extinción de dominio, entre otros. Es menester destacar su compromiso profesional con cada uno de sus procesos, su gran capacidad argumentativa y resolutiva en momentos clave del proceso.
               </p>
             </div>
           </div>
