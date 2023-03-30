1d5fb14d3f9f21d6be13585afa1b84dced4d9d9d
Maycol1490 <maicol1490@hotmail.com>
2023-03-30 11:12:24 -0500

    perfiles

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/About.css'
-import fotoPerfil from '../assets/logo-.jpeg'
+import amirPerfil from '../assets/amir_perfil.png'
 import camiloPerfil from '../assets/camilo_perfil.png'
 import mauricoPerfil from '../assets/mauricio_perfil.png'
 import noticia1 from '../assets/noticia1.mp4'
@@ -13,7 +13,7 @@ import noticia4 from '../assets/noticia4.mp4'
 const About = () => {
   return (
     <section>
-      <div>
+      <div className='principal'>
         <div className="card">
           <div className="card_load">
             <img src={camiloPerfil} alt="" />
@@ -46,7 +46,7 @@ const About = () => {
               </h1>
             </div>
             <div className="card_load_extreme_descripion">
-              <h4>Asociados</h4>
+              <h4>A bogado Asociado</h4>
               <p>
               Abogado egresado de la Universidad Libre, Litigante en derecho privado. Actualmente se desempeña como asesor externo de varias empresas de la región, es asesor principal externo de profesionales asociados empresa dedicada a la 
               recuperación de cartera y asesoría jurídica y administración de inmuebles, como profesional se destaca su empeño hacia el trabajo, capacidad de análisis y honestidad frente a las causas del cliente, gracias su experiencia cuenta con una alta valoración de sus clientes siendo un sello de respaldo para sus clientes.
@@ -58,16 +58,16 @@ const About = () => {
       <div>
         <div className="card">
           <div className="card_load">
-            <img src={fotoPerfil} alt="" />
+            <img src={amirPerfil} alt="" />
           </div>
           <div>
             <div className="card_load_extreme_title">
               <h1>
-                Amir Abdul Rahim Lopez 
+                Amir Abdul Rahim 
               </h1>
             </div>
             <div className="card_load_extreme_descripion">
-              <h4>Asociados</h4>
+              <h4> Abogado Asociado</h4>
               <p>
               Abogado egresado de la universidad Libre de Cali, especialista en Derecho Penal y Magister en criminalística y ciencias forenses de la misma universidad, Actualmente se desempeña como defensor de confianza en procesos penales en casos de conocimiento público, desarrollando defensas estratégicas sólidas a partir de pruebas periciales  científicas las cuales brindan plena garantía ante órganos de cierre como la Corte Suprema de Justicia, donde 
               ha tenido la oportunidad de manejar casos en delitos como Concierto para delinquir, narcotráfico, lavado de activos, porte de armas de uso privativo, tráfico de estupefacientes, terrorismo, delitos sexuales y procesos de extinción de dominio, entre otros. Es menester destacar su compromiso profesional con cada uno de sus procesos, su gran capacidad argumentativa y resolutiva en momentos clave del proceso.
