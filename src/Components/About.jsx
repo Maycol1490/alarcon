66fcf2c3aaf54b87808189baf38246ab60f1a472
Maycol1490 <maicol1490@hotmail.com>
2023-03-30 11:15:55 -0500

    .

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -42,11 +42,11 @@ const About = () => {
           <div>
             <div className="card_load_extreme_title">
               <h1>
-                Mauricio Martinez 
+                Mauricio Martinez. 
               </h1>
             </div>
             <div className="card_load_extreme_descripion">
-              <h4>A bogado Asociado</h4>
+              <h4>A bogado Asociado.</h4>
               <p>
               Abogado egresado de la Universidad Libre, Litigante en derecho privado. Actualmente se desempeña como asesor externo de varias empresas de la región, es asesor principal externo de profesionales asociados empresa dedicada a la 
               recuperación de cartera y asesoría jurídica y administración de inmuebles, como profesional se destaca su empeño hacia el trabajo, capacidad de análisis y honestidad frente a las causas del cliente, gracias su experiencia cuenta con una alta valoración de sus clientes siendo un sello de respaldo para sus clientes.
@@ -63,11 +63,11 @@ const About = () => {
           <div>
             <div className="card_load_extreme_title">
               <h1>
-                Amir Abdul Rahim 
+                Amir Abdul Rahim.
               </h1>
             </div>
             <div className="card_load_extreme_descripion">
-              <h4> Abogado Asociado</h4>
+              <h4> Abogado Asociado.</h4>
               <p>
               Abogado egresado de la universidad Libre de Cali, especialista en Derecho Penal y Magister en criminalística y ciencias forenses de la misma universidad, Actualmente se desempeña como defensor de confianza en procesos penales en casos de conocimiento público, desarrollando defensas estratégicas sólidas a partir de pruebas periciales  científicas las cuales brindan plena garantía ante órganos de cierre como la Corte Suprema de Justicia, donde 
               ha tenido la oportunidad de manejar casos en delitos como Concierto para delinquir, narcotráfico, lavado de activos, porte de armas de uso privativo, tráfico de estupefacientes, terrorismo, delitos sexuales y procesos de extinción de dominio, entre otros. Es menester destacar su compromiso profesional con cada uno de sus procesos, su gran capacidad argumentativa y resolutiva en momentos clave del proceso.
